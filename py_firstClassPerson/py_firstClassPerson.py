47b321157438a931eabf94c5227aa2589f35cec4
Stephen Zhou <zhoufwind@hotmail.com>
2014-06-09 22:58:34 -0700

    Created Class: Person

--- py_firstClassPerson/py_firstClassPerson.py
+++ py_firstClassPerson/py_firstClassPerson.py
@@ -1,1 +1,13 @@
-print('Hello World')
+__metaclass__ = type
+
+class Person:
+
+    def setName(self, name):
+        self.name = name
+
+    def getName(self):
+        return self.name
+
+    def greet(self):
+        print "Hello, world! I'm %s." % self.name
+
